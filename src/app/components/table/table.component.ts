0f7d450675431cfe5d33e63fb3abdd24b1e8989f
Teerasak <manu98765@hotmail.com>
2018-11-18 20:15:29 +0700

    edit

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
+  private prefix:any;
+  private tables:any;
   constructor() { }
 
   ngOnInit() {
+    this.prefix = ["นาย","นางสาว"];
+    this.tables = [
+                    {
+                      prefix: 'นางสาว',
+                      name: 'aaaaa',
+                      sername: 'bbbb',
+                    }
+                  ];
+  }
+
+  addData(){
+    this.tables.push({
+      prefix: '',
+      name: '',
+      sername: '',
+    });
+  }
+
+  delData(i){
+    //alert(i);
+    this.tables.splice(i,1);
   }
 
 }
