bfdf8e5cd0e56e6468ed746eb4fb845c58b0fb9c
Teerasak <manu98765@hotmail.com>
2018-11-18 20:06:31 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { SelectComponent } from './components/select/select.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent,
+    SelectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
